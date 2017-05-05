d7390f77ddadad73c23f5e51f8b99a37fa35916e
Danut Florian <daniesy@me.com>
2017-05-05 17:22:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index { |name, index| puts "#{index+1}.*#{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find { |ingredient| cheese_types.include?(ingredient) }
 end
+
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+puts summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+
+short_words = ["puff", "go", "two"]
+puts long_planeteer_calls(short_words)
+
+assorted_words = ["two", "go", "industrious", "bop"]
+puts long_planeteer_calls(assorted_words)
+
+snacks = ["crackers", "gouda", "thyme"]
+puts find_the_cheese(snacks)
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+puts find_the_cheese(soup)
+
+ingredients = ["garlic", "rosemary", "bread"]
+puts find_the_cheese(ingredients)
