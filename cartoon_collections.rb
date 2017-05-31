607a6f2b91882669ce9c7c9605b5d8c65188b3a6
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-05-31 22:43:19 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, ind|
+    puts "#{ind+1}. #{name}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planets)
+  planets.collect{|planet| planet.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(soups)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  soups.each {|soup| return soup if cheese_types.include?(soup)}
+  nil
 end
