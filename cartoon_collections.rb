fe61bac253ab7ec5025d717d2e516ec690061206
Austen <sean.a.mcgee@gmail.com>
2017-06-30 05:38:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index do |item, i|
+    puts "#{i+1} #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(list)
+  list.collect do |item|
+    item.sub(/^./, &:upcase) << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(list)
+  list.any? do |item|
+    item.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.each do |item|
+    if cheese_types.include?(item)
+      return item
+    end
+  end
+  return nil
 end
