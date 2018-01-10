ee7d4945f57553660f9c75c500c638aa95ca2c4e
Ivan Delaš <ivandelas@gmail.com>
2018-01-10 02:21:39 +0100

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = []
+
+def roll_call_dwarves(dwarves)
+  dwarves_enum = []
+  dwarves.each_with_index { |dwarf, index| dwarves_enum.push("#{index + 1}. #{dwarf}") }
+  puts dwarves_enum.join(" ")
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = []
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.each.map { |call| "#{call.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+calls = []
+
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+dairy_products = []
+
+def find_the_cheese(dairy_products)
+  cheese = ["cheddar", "gouda", "camembert"]
+  dairy_products.find { |product| cheese.include?(product) }
 end
