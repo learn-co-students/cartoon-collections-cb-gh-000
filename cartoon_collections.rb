49c6846c30e4688b1a2e1cfea33b3ea4578edf1a
Mars Wong <marswong618@gmail.com>
2018-01-26 03:50:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.collect.with_index { |x, i| print "#{i + 1}.*#{x}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map { |x| "#{x.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  result = arr.find { |x| x.length > 4 }
+  result ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find { |x| cheese_types.include?(x) }
 end
