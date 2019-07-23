f9cf3e5567310728c0e519e593af02f310643e8f
Carmen Maymo <mmaymo2@alumno.uned.es>
2019-07-23 17:06:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.enum_for(:each_with_index).each{|name, index| puts "#{index+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect{|name| "#{name.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.size > 4  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|word| word == cheese_types[0]|| word == cheese_types[1]||word == cheese_types[2]}
 end
